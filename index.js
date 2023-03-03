f93d6e7b2ce16a2463d887114cdf6357c0a7b4c9
Min Ho <minho00123@gmail.com>
2023-03-03 19:32:01 +0900

    Update week box and progress bar

--- index.js
+++ index.js
@@ -29,3 +29,59 @@ if (hr >= 12) {
   document.getElementById('pm').style.color = "gray";
 }
 
+// Calculate this week
+const today = [date.getFullYear(), date.getMonth(), date.getDate()];
+const dayInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 29];
+let days = 0;
+
+for (let i = 0; i < today[1]; i++) {
+  if (i === 1) {
+    if (today[0] % 400 === 0) {
+      days += dayInMonth[12];
+    } else if (today[0] % 4 === 0 && today[0] % 100 !== 0) {
+      days += dayInMonth[12];
+    } else {
+      days += dayInMonth[i];
+    }
+  } else {
+    days += dayInMonth[i];
+  }
+}
+days += today[2];
+let date_num = 0;
+if (date.getDay() === 0) {
+  date_num = 7;
+} else {
+  date_num = date.getDay();
+}
+let weekNum = Math.floor((days - date_num + 10) / 7); // https://annyeong.me/notes/%EC%98%A4%EB%8A%98%EC%9D%80-%EB%AA%87-%EC%A3%BC%EC%B0%A8/ 참고
+
+if (weekNum === 0) {
+  document.getElementById('week_num').innerHTML = 52;
+} else {
+  document.getElementById('week_num').innerHTML = weekNum;
+}
+
+// Calculate the week of days in a month
+
+function calculateMonthWeekNum (date) {
+let monthWeekNum = Math.floor((date.getDate() - date_num + 10) / 7);
+
+  return monthWeekNum;
+}
+
+if (date.getMonth() + 1 === 1 && calculateMonthWeekNum(date) === 0) {
+  document.getElementById('month_week').innerHTML = 12;
+  document.getElementById('month_week_num').innerHTML = 5;
+} else if (calculateMonthWeekNum(date) === 0) {
+  document.getElementById('month_week').innerHTML = date.getMonth();
+  document.getElementById('month_week_num').innerHTML = calculateMonthWeekNum(new Date(date.getFullYear(), date.getMonth(), 26));
+} else {
+  document.getElementById('month_week').innerHTML = date.getMonth() + 1;
+  document.getElementById('month_week_num').innerHTML = calculateMonthWeekNum(date);
+}
+
+// Calculate the percentage of the progress bar
+const todayPercentage = Math.round(days / 365 * 100);
+document.getElementById('progress').setAttribute('value', todayPercentage);
+
